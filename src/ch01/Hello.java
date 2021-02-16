d2fe1c5585ffb8b2adec79d0a2561e1728d75afa
kju1018 <kms951018@naver.com>
2021-02-16 15:13:31 +0900

    코드 수정

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -4,13 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java1");
-		System.out.println("Hello Java2");
-		System.out.println("Hello Java3");
-		System.out.println("Hello Java4");
-		System.out.println("Hello Java5");
-		System.out.println("Hello Java6");
-		System.out.println("Hello Java7");
-		System.out.println("Hello Java8");
+		
 	}
 
 }
