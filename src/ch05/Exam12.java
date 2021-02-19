0cd6e2eccb3d8de135bf4635e1dd86b6b18c5d30
KIM M S <61860897+kju1018@users.noreply.github.com>
2021-02-19 10:27:35 +0900

    코드 수정

--- src/ch05/Exam12.java
+++ src/ch05/Exam12.java
@@ -3,7 +3,6 @@ package ch05;
 public class Exam12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Season nowSeason = Season.WINTER;
 		if(nowSeason == Season.SUMMER) {
 			System.out.println("맞습니다.");
@@ -13,9 +12,9 @@ public class Exam12 {
 		}
 		LoginResult loginResult = LoginResult.SUCCESS;
 		if(loginResult == LoginResult.SUCCESS) {
-			System.out.println("로그인 성공했군요.");
+			System.out.println("로그인 성공.");
 		}else if(loginResult == LoginResult.FAILURE_ID) {
-			System.out.println("아이디를 다시 입력해 주시에ㅛ");
+			System.out.println("아이디를 다시 입력해 주세요");
 		}else {
 			System.out.println("패스워드가 틀립니다.");
 		}
