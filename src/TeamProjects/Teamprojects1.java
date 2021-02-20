9b10d1f7dc347bb0bf169a7fa1223bb5bdfea2a2
kju1018 <kms951018@naver.com>
2021-02-20 18:59:34 +0900

    팀프로젝트

--- src/TeamProjects/Teamprojects1.java
+++ src/TeamProjects/Teamprojects1.java
@@ -7,18 +7,9 @@ public class Teamprojects1 {
 	public static void main(String[] args) {
 		boolean run = true;
 		String[][] boardArray = new String[100][5];
-		// 모든 boardArray[0 ~ 99]까지는 생성 안에 내용은 null
-		// boardArray[0 ~ 99] = null로 검사할시 number 이후는 null이 아님
 		Scanner scanner = new Scanner(System.in);
-		int number = 4;// 다음 번호
-
-		for (int i = 0; i < 3; i++) {
-			boardArray[i][0] = "" + (i + 1);
-			boardArray[i][1] = "title" + (i + 1);
-			boardArray[i][2] = "content" + (i + 1);
-			boardArray[i][3] = "name" + (i + 1);
-			boardArray[i][4] = "0";
-		}
+		int number = 1;// 다음 번호
+		int count = 0;
 
 		while (run) {
 			System.out.println("--------------------------------------------------------------------------");
@@ -26,11 +17,11 @@ public class Teamprojects1 {
 			System.out.println("--------------------------------------------------------------------------");
 			System.out.print("메뉴선택> ");
 
-			int selectMenu = Integer.parseInt(scanner.nextLine());
+			String selectMenu = scanner.nextLine();
 
-			if (selectMenu == 1) {
+			if (selectMenu.equals("1")) {
 
-			} else if (selectMenu == 2) {
+			} else if (selectMenu.equals("2")) {
 
 				System.out.print("제목: ");
 				String title = scanner.nextLine();
@@ -47,10 +38,11 @@ public class Teamprojects1 {
 						boardArray[i][3] = name;
 						boardArray[i][4] = String.valueOf(0);
 						number++;
+						count++;
 						break;
 					}
 				}
-			} else if (selectMenu == 3) {
+			} else if (selectMenu.equals("3")) {
 				System.out.print("번호: ");
 				String selectNumber = scanner.nextLine();
 
@@ -67,11 +59,14 @@ public class Teamprojects1 {
 							break;
 						}
 					}
+					if (i == 99) {
+						System.out.println("해당 번호의 글이 없습니다.");
+					}
 				}
 				System.out.println();
 				continue;
 
-			} else if (selectMenu == 4) {
+			} else if (selectMenu.equals("4")) {
 				System.out.println();
 				System.out.print("번호: ");
 				String selectNumber = scanner.nextLine();
@@ -86,7 +81,7 @@ public class Teamprojects1 {
 							if (!editTitle.equals("")) {
 								boardArray[i][1] = editTitle;
 							}
-
+							
 							System.out.println("기존내용: " + boardArray[i][2]);
 							System.out.print("수정내용: ");
 							String editContent = scanner.nextLine();
@@ -96,12 +91,12 @@ public class Teamprojects1 {
 							break;
 						}
 					}
-					if(i == 99) {
+					if (i == 99) {
 						System.out.println("해당 번호의 글이 없습니다.");
 					}
 				}
 
-			} else if (selectMenu == 5) {
+			} else if (selectMenu.equals("5")) {
 				System.out.println();
 				System.out.print("번호: ");
 				String selectNumber = scanner.nextLine();
@@ -112,65 +107,61 @@ public class Teamprojects1 {
 							for (int k = 0; k < 5; k++) {
 								boardArray[i][k] = null;
 							}
+							count--;
 							break;
 						}
 					}
+					if (i == 99) {
+						System.out.println("해당 번호의 글이 없습니다.");
+					}
 				}
 
-			} else if (selectMenu == 6) {
+			} else if (selectMenu.equals("6")) {
 				break;
 			} else {
 				System.out.println();
 				continue;
 			}
 			System.out.println();
-			System.out.println("----------------------------------------------------------------------");
-			System.out.println("   번호   |     제목     |          내용          |   글쓴이   |   조회수   ");
-			System.out.println("----------------------------------------------------------------------");
+			System.out.println("------------------------------------------------------------------------------------");
+			System.out.println("\t번호\t |\t제목\t |\t내용\t |\t글쓴이\t |\t조회수\t ");
+			System.out.println("------------------------------------------------------------------------------------");
 
 			String[][] tempArray = new String[100][5];
 
 			System.arraycopy(boardArray, 0, tempArray, 0, tempArray.length);
-
-			String[] temp; 
-			for (int i = 0; i < 99; i++) {
-				for (int k = i+1; k < 100; k++) {
-					if(tempArray[i][0] != null && tempArray[k][0] != null) {
-						if(Integer.parseInt(tempArray[i][0]) > Integer.parseInt(tempArray[k][0])) {
-							System.out.println(tempArray[i][0] + "   " + tempArray[k][0]);
-							temp = tempArray[k];
-							tempArray[k] = tempArray[i];
-							tempArray[i] = temp;
+			
+			String[] temp;
+			if (count <= 0) {
+				System.out.println("글이 없습니다.");
+			} else {
+				for (int i = 0; i < 99; i++) {
+					for (int k = i + 1; k < 100; k++) {
+						if (tempArray[i][0] != null && tempArray[k][0] != null) {
+							if (Integer.parseInt(tempArray[i][0]) > Integer.parseInt(tempArray[k][0])) {
+								temp = tempArray[k];
+								tempArray[k] = tempArray[i];
+								tempArray[i] = temp;
+							}
 						}
 					}
 				}
-			}
 
-			for (int i = 99; i >= 0; i--) {
-				if (tempArray[i][0] != null) {
-					for (String value : tempArray[i]) {
-						System.out.print(value + "\t");
+				for (int i = 99; i >= 0; i--) {
+					if (tempArray[i][0] != null) {
+						for (String value : tempArray[i]) {
+							System.out.print("\t" + value + "\t");
+						}
+						System.out.println();
 					}
-					System.out.println();
 				}
 			}
 			System.out.println();
-						
-			for (int i = 99; i >= 0; i--) {
-				if (boardArray[i][0] != null) {
-					System.out.print(i + "      ");
-					for (String value : boardArray[i]) {
-						System.out.print(value + "\t");
-					}
-					System.out.println();
-				}
-			}
-			System.out.println();
+
 		}
 
 		System.out.println("프로그램 종료");
 
 	}
-
 
 }
