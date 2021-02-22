e4c5de2479097e6a899ce7340b5dd75b2094c08b
kju1018 <kms951018@naver.com>
2021-02-22 09:50:02 +0900

    주석

--- src/TeamProjects/Teamprojects1.java
+++ src/TeamProjects/Teamprojects1.java
@@ -8,8 +8,8 @@ public class Teamprojects1 {
 		boolean run = true;
 		String[][] boardArray = new String[100][5];
 		Scanner scanner = new Scanner(System.in);
-		int number = 1;// 다음 번호
-		int count = 0; // 개수
+		int number = 1;// 글의 번호
+		int count = 0; // 글의 개수
 
 		while (run) {
 			System.out.println("--------------------------------------------------------------------------");
@@ -20,7 +20,7 @@ public class Teamprojects1 {
 			String selectMenu = scanner.nextLine();
 
 			if (selectMenu.equals("1")) {
-
+					
 			} else if (selectMenu.equals("2")) {
 				System.out.print("제목: ");
 				String title = scanner.nextLine();
@@ -121,20 +121,22 @@ public class Teamprojects1 {
 				System.out.println();
 				continue;
 			}
+			
 			System.out.println();
 			System.out.println("------------------------------------------------------------------------------------");
 			System.out.println("\t번호\t |\t제목\t |\t내용\t |\t글쓴이\t |\t조회수\t ");
 			System.out.println("------------------------------------------------------------------------------------");
 
-			String[][] tempArray = new String[100][5];
+		
+			String[][] tempArray = new String[100][5];	//정렬을 위한 배열
 
-			System.arraycopy(boardArray, 0, tempArray, 0, tempArray.length);
+			System.arraycopy(boardArray, 0, tempArray, 0, tempArray.length);//배열 복사
 
 			String[] temp;
-			if (count <= 0) {
+			if (count <= 0) {//글의 개수가 없을경우
 				System.out.println("글이 없습니다.");
 			} else if(count < 100){
-				for (int i = 0; i < 99; i++) {
+				for (int i = 0; i < 99; i++) {//정렬을 위한 코드
 					for (int k = i + 1; k < 100; k++) {
 						if (tempArray[i][0] != null && tempArray[k][0] != null) {
 							if (Integer.parseInt(tempArray[i][0]) > Integer.parseInt(tempArray[k][0])) {
@@ -145,7 +147,7 @@ public class Teamprojects1 {
 						}
 					}
 				}
-
+				//출력
 				for (int i = 99; i >= 0; i--) {
 					if (tempArray[i][0] != null) {
 						for (String value : tempArray[i]) {
